94ec13ff12bcc9dcc93a2e26bfb9354066d444b4
LucasGabrielqs <lucasgabriel20032013@gmail.com>
2024-12-13 12:34:21 -0300

    ajuste final

--- busca/views.py
+++ busca/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Produto,Fornecedor,Categoria
 
 # Create your views here.
+
+def index(request):
+    produto = Produto.objects.all()
+    fornecedor = Fornecedor.objects.all()
+    categoria = Categoria.objects.all()
+    context = {'produtos':produto,'fornecedores':fornecedor,'categorias':categoria}
+    return render(request, 'busca/index.html',context)
+
+def details(request,id):
+    produto = Produto.objects.get(id=id)
+    return render(request,'busca/detalhes.html',{'produto':produto})
+
